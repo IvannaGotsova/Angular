45f0f4ffc86c970f32ef48a75824997e62e68011
IvannaGotsova <ivannagotsova@outlook.com>
2024-03-27 23:06:02 +0200

    Added imports in app.component.ts

--- first-example/src/app/app.component.ts
+++ first-example/src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ExampleComponent } from './components/example/example.component';
+import { HeaderComponent } from './components/header/header.component';
+import { BodyComponent } from './components/body/body.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ExampleComponent],
+  imports: [RouterOutlet, HeaderComponent, BodyComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
