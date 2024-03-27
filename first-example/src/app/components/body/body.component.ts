4f40f178bdb3ef30bdcb41a7084a17a0d5609511
IvannaGotsova <ivannagotsova@outlook.com>
2024-03-27 23:38:00 +0200

    Imported component for the button

--- first-example/src/app/components/body/body.component.ts
+++ first-example/src/app/components/body/body.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { ButtonComponent } from '../button/button.component';
 
 @Component({
   selector: 'app-body',
   standalone: true,
-  imports: [],
+  imports: [ButtonComponent],
   templateUrl: './body.component.html',
   styleUrl: './body.component.css'
 })
