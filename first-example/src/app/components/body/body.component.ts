6b4fd9e011d1eab6584f2eeacfc5a57e7087771b
IvannaGotsova <ivannagotsova@outlook.com>
2024-03-31 22:08:14 +0300

    Added import fot OutputParentComponent

--- first-example/src/app/components/body/body.component.ts
+++ first-example/src/app/components/body/body.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { ButtonComponent } from '../button/button.component';
 import { InputComponent } from '../input/input.component';
 import { InputParentComponent } from '../input-parent/input-parent.component';
+import { OutputParentComponent } from '../output-parent/output-parent.component';
 
 @Component({
   selector: 'app-body',
   standalone: true,
-  imports: [ButtonComponent, InputComponent, InputParentComponent],
+  imports: [ButtonComponent, InputComponent, InputParentComponent, OutputParentComponent],
   templateUrl: './body.component.html',
   styleUrl: './body.component.css'
 })
