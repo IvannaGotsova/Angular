dd5f310a0a18df96355d7b835ab37fe696545723
IvannaGotsova <ivannagotsova@outlook.com>
2024-03-31 21:32:45 +0300

    Added import of InputParentComponent

--- first-example/src/app/app.component.ts
+++ first-example/src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { BodyComponent } from './components/body/body.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { InputParentComponent } from './components/input-parent/input-parent.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, BodyComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, BodyComponent, FooterComponent, InputParentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
