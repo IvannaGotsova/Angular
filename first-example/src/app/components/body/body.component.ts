4ed561fa880646e448b3f2c1998a9dfdc03eae78
IvannaGotsova <ivannagotsova@outlook.com>
2024-03-31 21:38:19 +0300

    Corrected the right imports

--- first-example/src/app/components/body/body.component.ts
+++ first-example/src/app/components/body/body.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { ButtonComponent } from '../button/button.component';
 import { InputComponent } from '../input/input.component';
+import { InputParentComponent } from '../input-parent/input-parent.component';
 
 @Component({
   selector: 'app-body',
   standalone: true,
-  imports: [ButtonComponent, InputComponent],
+  imports: [ButtonComponent, InputComponent, InputParentComponent],
   templateUrl: './body.component.html',
   styleUrl: './body.component.css'
 })
