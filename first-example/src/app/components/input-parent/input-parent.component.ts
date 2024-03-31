178fa3f9b2dcff0bcc15f59a8c62e99b685790e9
IvannaGotsova <ivannagotsova@outlook.com>
2024-03-31 21:29:37 +0300

    Created new component input-parent

---
+++ first-example/src/app/components/input-parent/input-parent.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-input-parent',
+  standalone: true,
+  imports: [],
+  templateUrl: './input-parent.component.html',
+  styleUrl: './input-parent.component.css'
+})
+export class InputParentComponent {
+
+}
