420695464f73faac06e8ff46415b7034c05ff0b8
IvannaGotsova <ivannagotsova@outlook.com>
2024-03-31 22:01:46 +0300

    Created output-parent component

---
+++ first-example/src/app/components/output-parent/output-parent.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-output-parent',
+  standalone: true,
+  imports: [],
+  templateUrl: './output-parent.component.html',
+  styleUrl: './output-parent.component.css'
+})
+export class OutputParentComponent {
+
+}
