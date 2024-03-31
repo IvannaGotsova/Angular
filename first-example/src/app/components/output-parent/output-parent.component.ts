c67f4dcece7b55b80bac0b3c2d33f2f4c2dcf14f
IvannaGotsova <ivannagotsova@outlook.com>
2024-03-31 22:08:56 +0300

    Added import OutputChildComponent

--- first-example/src/app/components/output-parent/output-parent.component.ts
+++ first-example/src/app/components/output-parent/output-parent.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { OutputChildComponent } from '../output-child/output-child.component';
 
 @Component({
   selector: 'app-output-parent',
   standalone: true,
-  imports: [],
+  imports: [OutputChildComponent],
   templateUrl: './output-parent.component.html',
   styleUrl: './output-parent.component.css'
 })
