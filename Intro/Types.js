bf5bc47a6c8902002647f19d6ce02b80b920cae1
IvannaGotsova <ivannagotsova@outlook.com>
2025-10-11 16:30:27 +0300

    TS Types

---
+++ Intro/Types.js
@@ -0,0 +1,5 @@
+var string = "Ivan";
+var number = 30;
+var isTrue = true;
+var example = "The string is ".concat(string, ", the numbers is ").concat(number, ", the bool is ").concat(isTrue, ".");
+console.log(example);
---
+++ Intro/Types.ts
@@ -0,0 +1,7 @@
+let string: string = "Ivan";
+let number: number = 30;
+let isTrue: boolean = true;
+
+let example: string = `The string is ${string}, the numbers is ${number}, the bool is ${isTrue}.`;
+
+console.log(example)
