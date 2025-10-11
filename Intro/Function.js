44eb1e4d762c50fc47ba82b84fa9fa5f93c4cc5f
IvannaGotsova <ivannagotsova@outlook.com>
2025-10-11 16:29:43 +0300

    TS Function

---
+++ Intro/Function.js
@@ -0,0 +1,8 @@
+function printHello() {
+    console.log('Hello World!');
+}
+function sum(a, b) {
+    return a + b;
+}
+printHello();
+console.log(sum(1, 2));
---
+++ Intro/Function.ts
@@ -0,0 +1,12 @@
+function printHello(): void {
+  console.log('Hello World!');
+}
+
+function sum(a: number, b: number) {
+  return a + b;
+}
+
+
+
+printHello();
+console.log(sum(1, 2));
