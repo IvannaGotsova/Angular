58c40caba74c2cf4f47d37d5f9087f4ba300ee52
IvannaGotsova <ivannagotsova@outlook.com>
2025-10-11 16:30:08 +0300

    TS Object

---
+++ Intro/Object.js
@@ -0,0 +1,8 @@
+var person = {
+    name: "Ivan",
+    town: "Sofia",
+    age: 33
+};
+console.log(person.name);
+console.log(person.town);
+console.log(person.age);
---
+++ Intro/Object.ts
@@ -0,0 +1,10 @@
+const person: { name: string, town: string, age: number } = {
+  name: "Ivan",
+  town: "Sofia",
+  age: 33
+};
+
+
+console.log(person.name)
+console.log(person.town)
+console.log(person.age)
