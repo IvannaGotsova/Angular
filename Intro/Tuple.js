a15e2ffdd83c44c5ba2e9280693af5f16e3ccc42
IvannaGotsova <ivannagotsova@outlook.com>
2025-10-11 16:30:18 +0300

    TS Tupple

---
+++ Intro/Tuple.js
@@ -0,0 +1,5 @@
+var list = ["Name", 22, true];
+for (var index = 0; index < list.length; index++) {
+    var element = list[index];
+    console.log(element);
+}
---
+++ Intro/Tuple.ts
@@ -0,0 +1,6 @@
+const list : [string, number, boolean] = ["Name", 22, true]
+
+for (let index = 0; index < list.length; index++) {
+    const element = list[index];
+    console.log(element)
+}
