81dd966afdd5357a7cb8c08fbcf7017608d0f9e6
IvannaGotsova <ivannagotsova@outlook.com>
2025-10-12 14:02:16 +0300

    Hello World

--- Angular-Beginner-app/src/main.ts
+++ Angular-Beginner-app/src/main.ts
@@ -1,6 +1,11 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
+import { Component } from '@angular/core';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  template: `<h1>Hello, World!</h1>`
+})
+class App {}
+
+bootstrapApplication(App);
