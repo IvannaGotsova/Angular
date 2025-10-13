9bf3b594d8a8525b4cc528fc2d1f054662523ee3
IvannaGotsova <ivannagotsova@outlook.com>
2025-10-13 20:28:26 +0300

    Added pipe

--- Angular-Beginner-app/src/main.ts
+++ Angular-Beginner-app/src/main.ts
@@ -1,17 +1,19 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
+  imports: [CommonModule, FormsModule],
   template: `
     <h3>{{ title }}</h3>
     <input [(ngModel)]="current" type="text" placeholder="Type Name" />
     <button (click)="read()">Change Name</button>
-    <p>Hello {{ currentName }}!</p>
-  `,
-  imports: [FormsModule]
+    <p>Hello {{ currentName | uppercase }}!</p>
+  `
 })
 export class App {
   title = 'Hello User!';
