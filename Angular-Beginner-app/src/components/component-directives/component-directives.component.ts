afde4e36451482777cfb243960e16be5e4c7e27d
IvannaGotsova <ivannagotsova@outlook.com>
2025-10-24 16:48:45 +0300

    Component Directive

---
+++ Angular-Beginner-app/src/components/component-directives/component-directives.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-component-directives',
+  imports: [],
+  templateUrl: './component-directives.component.html',
+  styleUrl: './component-directives.component.css'
+})
+export class ComponentDirectivesComponent {
+
+}
