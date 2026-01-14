779239630b51adbb733524c45ed7946dae1694a1
IvannaGotsova <ivannagotsova@outlook.com>
2026-01-14 21:57:02 +0200

    Dynamic

---
+++ Angular-Advanced-app/src/components/dynamic-component/dynamic-component.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dynamic-component',
+  imports: [],
+  templateUrl: './dynamic-component.component.html',
+  styleUrl: './dynamic-component.component.css'
+})
+export class DynamicComponentComponent {
+
+}
